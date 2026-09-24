7c685357051e612fc7a4e169ae7694fbf89272d5
115222034 <a115222034@mail.shu.edu.tw>
2026-09-24 17:56:51 +0800

    2

---
+++ 115_09_24/Tutorial_2_2/Tutorial_2_2/Form1.cs
@@ -0,0 +1,15 @@
+namespace Tutorial_2_2
+{
+    public partial class messageButton : Form
+    {
+        public messageButton()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("一閃一閃亮晶晶，滿天都是小星星。");
+        }
+    }
+}
